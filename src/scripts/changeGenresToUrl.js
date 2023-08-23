61306033ad34b9306d130b40a288f28d7de497bd
Visal512 <142089936+Visal512@users.noreply.github.com>
2023-08-23 20:25:02 +0100

    Create changeGenresToUrl.js

---
+++ src/scripts/changeGenresToUrl.js
@@ -0,0 +1,8 @@
+const changeGenresToUrl = (selectedGenres) => {
+  if (selectedGenres.length < 1) return "";
+
+  const GenreIds = selectedGenres.map((g) => g.id);
+  return GenreIds.reduce((acc, curr) => acc + "," + curr);
+};
+
+export default changeGenresToUrl;
